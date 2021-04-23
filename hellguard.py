14cacc8ab42a80ffd269feb963481f431253db6f
rakin406 <rakinhacker@gmail.com>
2021-04-23 17:41:42 +0600

    Hide gui

--- hellguard.py
+++ hellguard.py
@@ -25,16 +25,6 @@ while True:
             flags=cv2.CASCADE_SCALE_IMAGE
             )
 
-    # Draw a rectangle around the faces
-    for (x, y, w, h) in faces:
-        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
-
-    # Display the resulting frame
-    cv2.imshow('Video', frame)
-
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-
 # When everything is done, release the capture
 video_capture.release()
 cv2.destroyAllWindows()
