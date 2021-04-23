9c51c7bc0a75c96f5f7e70d99c6a03172466b9a1
rakin406 <rakinhacker@gmail.com>
2021-04-23 17:30:19 +0600

    Face detection

--- hellguard.py
+++ hellguard.py
@@ -1,1 +1,40 @@
 #!/usr/bin/env python3
+"""
+Home surveillance
+"""
+
+import sys
+import cv2
+
+CASC_PATH = sys.argv[1]
+FACE_CASCADE = cv2.CascadeClassifier(CASC_PATH)
+
+video_capture = cv2.VideoCapture(0)
+
+while True:
+    # Capture frame-by-frame
+    ret, frame = video_capture.read()
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    faces = FACE_CASCADE.detectMultiScale(
+            gray,
+            scaleFactor=1.1,
+            minNeighbors=5,
+            minSize=(30, 30),
+            flags=cv2.cv.CV_HAAR_SCALE_IMAGE
+            )
+
+    # Draw a rectangle around the faces
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+    # Display the resulting frame
+    cv2.imshow('Video', frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# When everything is done, release the capture
+video_capture.release()
+cv2.destroyAllWindows()
